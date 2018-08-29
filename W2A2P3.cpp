3926c5c2bd8f4182fabb63a4174ca09ba5d1b7a2
Tushar Sharma <imtusharsharma@gmail.com>
2018-08-30 00:51:05 +0530

    Add File
    Completing formal Arguments

---
+++ W2A2P3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int swap(int &y , int &z) {		//complete formal arguements
+    int t;
+    
+    t = y;
+    y = z;
+    z = t;
+    
+    return 0;
+}
+int main() {
+
+    int a, b;
+    
+    cin >> a >> b;
+    
+    swap(a, b);
+    
+    cout << a << " " << b << endl;
+    
+    return 0;
+}
