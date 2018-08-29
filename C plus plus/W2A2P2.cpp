f521f9f3c570528fef2b68ce0eb73c22be24f73a
Tushar Sharma <imtusharsharma@gmail.com>
2018-08-30 00:59:00 +0530

    Create W2A2P2.cpp
    Completing macro definition

---
+++ C plus plus/W2A2P2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define SQUARE(x) (x)*(x)-(x-1) // Complete the Macro definition
+ int main() {
+     int i, j;
+    
+    cin >> i;
+    
+    j = SQUARE(i + 1); 
+    
+    cout << j << endl;
+    
+    return 0;
+}
