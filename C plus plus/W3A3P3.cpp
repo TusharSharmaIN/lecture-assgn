59c6807f8d380563a80907b9146245cb99e65df9
Tushar Sharma <imtusharsharma@gmail.com>
2018-09-06 22:37:58 +0530

    Create W3A3P3.cpp
    Queue Implementation

---
+++ C plus plus/W3A3P3.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+class Queue {
+    private : 
+
+        vector<char> data_;
+
+        int front_, rear_;
+    public :
+        Queue():front_(-1), rear_(-1)  { data_.resize(20); }             //Write the missing statement
+
+        ~Queue() {};
+
+        int empty() {
+            if (rear_ < front_ || (rear_ == -1 && front_ == -1)){       //Write the missing statement    
+
+                front_ = rear_ = -1 ;
+                return 1;
+            }
+            else
+                return 0;
+        }
+        void add(char x) {
+
+            data_[++rear_]=x;                                           //Write the missing statement
+
+            if (front_ == -1)
+               
+                front_++;                                               //Write the missing statement
+            
+        }
+        void del( ) {   
+						if(!empty())   front_++;                                    //Write the missing statement
+        }                                                               
+
+        char front() { return data_[front_] ; }                         //Write the missing statement
+
+} ; // End of the class
+
+int main( ) {
+    Queue q;
+    char str[20];
+    
+    cin >> str;
+    for (int i = 0; i < strlen(str); ++i)
+
+        q.add(str[i]);
+        
+    while (!q.empty()) {
+
+        cout << q.front();
+
+        q.del();
+    }
+    
+    return 0;
+}
