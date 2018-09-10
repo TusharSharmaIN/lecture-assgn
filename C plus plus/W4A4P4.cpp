4fa4c3ce68d749005b74650c90f4d9a0e138d393
Tushar Sharma <imtusharsharma@gmail.com>
2018-09-10 22:02:53 +0530

    Create W4A4P4.cpp
    Complete class definition - operator overloading

---
+++ C plus plus/W4A4P4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Date {
+        int dd; 
+        int mm;
+        int yyyy;
+
+    public:
+
+        Date(int d, int m, int y): dd(d), mm(m), yyyy(y) { }
+        
+        friend ostream& operator<<(ostream& os, const Date& dt); // Fill the return type
+
+}; // End of the class
+
+ostream& operator<<(ostream& os, const Date& dt) {  // Fill the appropriate return type
+
+    os<<dt.mm<<"/"<<dt.dd<<"/"<<dt.yyyy;    // Write the implementation code looking to the O/P format
+
+    return os;
+}
+
+int main() {
+    int day, month, year;
+    cin >> day;
+    cin >> month;
+    cin >> year;
+    
+    Date dt(day, month, year);
+    cout << dt;
+    
+    return 0;
+}
