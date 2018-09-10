fc669276b39ea6a640d0f66c94ef2fa14d64dd56
Tushar Sharma <imtusharsharma@gmail.com>
2018-09-10 20:50:02 +0530

    Create W4A4P2.cpp
    Complete class definition

---
+++ C plus plus/W4A4P2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class MyClass {
+
+    static int x;
+
+    public:
+        void get() { x++; }
+        
+static void print(int y) { // Fill the blanks with proper keywords
+
+            x = x - y;
+
+            cout << x << " ";
+        }
+};
+
+int MyClass::x=1;  // Define static data member
+
+int main() {
+    int x;
+    
+    cin >> x;
+    
+    MyClass::print(x);
+    MyClass s1;
+    
+    s1.get();
+    s1.print(x);
+    
+    return 0;
+}
