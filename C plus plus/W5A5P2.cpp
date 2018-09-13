aaef859e75e8356eb3d662818bc137c06d99808f
Tushar Sharma <imtusharsharma@gmail.com>
2018-09-13 21:31:57 +0530

    Create W5A5P2.cpp
    Complete Class Definition - Inheritance

---
+++ C plus plus/W5A5P2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Flower {
+public:
+    string name;
+
+    Flower(string n) : name(n) { }
+
+    void getFlowerName() { cout << name << " " << "is" << " "; }
+
+};
+
+class Rose: private Flower { // Inherit Flower as private
+public:
+    string color;
+
+    /* Initialize name and color data members */
+    Rose(string n = "No flower", string c = "Red") : Flower(n), color(c) {}
+    
+    void getFlowerName(Rose& r) { cout << r.color << endl; }
+    
+    void getFlowerName() { cout << name << " " << "is" << " "; }    // Allow call to  getFlowerName() method in the base class
+};
+
+int main() {
+    string flr, clor;
+
+    cin >> flr >> clor;
+
+    Rose s(flr);
+    Rose r(flr, clor);
+
+    s.getFlowerName();
+    s.getFlowerName(r);
+
+    return 0;
+}
