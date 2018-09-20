4e83296b1fe6b2a2b65028cda92e128a3ee3e7ab
Tushar Sharma <imtusharsharma@gmail.com>
2018-09-20 23:54:18 +0530

    Create W6A6P2.cpp
    Write function definition - virtual member function

---
+++ C plus plus/W6A6P2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+    protected: 
+
+        int width, height;
+
+    public:
+           virtual int getArea() { return (width * height); }
+             // Define the getArea() function here
+   void setWidth(int w) { width = w; }
+        void setHeight(int h) { height = h; }
+};
+
+class Rectangle : public Shape {
+    public:
+        int getArea() { return (width * height); }
+};
+
+class Triangle : public Shape {
+    public:
+        int getArea() { return (width * height) / 2; }
+};
+
+int main(void) {
+    int x, y;
+    cin >> y >> x;
+    
+    Rectangle Rect;
+    Triangle  Tri;
+
+    Rect.setWidth(x);
+    Rect.setHeight(y);
+
+    Tri.setWidth(x);
+    Tri.setHeight(y);
+
+    Shape *shape[] = { &Rect, &Tri, 0 };
+    Shape **pShape = &shape[0];
+
+    while (*pShape) 
+        cout << (*pShape++)->getArea() << " ";
+
+    return 0;
+}          
+   
