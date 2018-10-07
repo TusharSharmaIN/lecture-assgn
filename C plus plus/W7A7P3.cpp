777d20a1360c822cab33ffd284c27f3c2479a6dc
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-07 23:11:31 +0530

    Create W7A7P3.cpp
    Complete class definition - Cast Operator

---
+++ C plus plus/W7A7P3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class student {
+    private: 
+        int roll;
+
+    public:
+        student(int r) :roll(r) {}
+        
+        void fun() const {
+
+            (const_cast<student*>(this))->roll = 5;                     //Blank
+
+        }
+int getRoll()  { return roll; }
+
+}; // End of class student
+
+int main() {
+
+    int old_roll_no = 0;
+    
+    cin >> old_roll_no;
+    student s(old_roll_no);
+    
+    cout << s.getRoll() << " ";
+    s.fun();
+    
+    cout << s.getRoll();
+    
+    return 0;
+}
