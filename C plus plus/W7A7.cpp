6a09cd46307c79e5dccb114f0b71a909384ee971
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-07 23:07:03 +0530

    Create W7A7.cpp
    Complete Class Definition - Virtual function for Dynamic Binding

---
+++ C plus plus/W7A7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class A {
+    protected:
+        int i;
+    public:
+        A(int a = 0): i(a) {}
+        virtual void fun() {                            //Blank
+            cout << i; // Don't Edit this line
+        }
+}; // End of class A
+
+class B: public A {
+    public:
+        B(int a = 0): A(a) {}
+        void fun() {                                    //Blank
+            cout << i * i; // Don't Edit this line
+        } 
+}; // End of class B
+
+class C: public B {
+public:
+        C(int a = 0): B(a) {}
+        void fun() { cout << i + i; }
+};// End of class C
+
+int main() {
+    int num;
+    cin >> num;
+    B *bp = new C(num);
+    bp->fun();
+    return 0;
+}
