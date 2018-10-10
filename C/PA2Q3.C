9f2725e6852fd4b24c1c2edc00a40fb5b586aff4
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 21:59:28 +0530

    Problem Statement Updated
    Count Distinct Numbers in an Array

--- C/PA2Q3.C
+++ C/PA2Q3.C
@@ -1,11 +1,22 @@
+/*   
+ *	Problem Statement
+ *
+ *	You are given a sorted (either in the increasing or in the decreasing order) sequence of numbers, ending with a -1. You can assume that are at least two numbers before the ending -1. 
+ *	
+ *	Let us call the sequence x0 x1 ... xn -1.
+ *	
+ *	You have to output the number of distinct elements in the sorted sequence.
+ *	
+ *	Kindly do not use arrays in the code. 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int min=0, num, count=0;
-
-    scanf("%d",&num);
+    	int min=0, num, count=0;
+    	scanf("%d",&num);
 
 	while(num!=-1)
 	{
@@ -14,8 +25,7 @@ int main()
         min=num;
         scanf("%d",&num);
 	}
-
+	
 	printf("%d\n",count);
-    
 	return 0;
 }
