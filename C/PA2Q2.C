35af0b1bc3fb517bb9923407aeb9f6968ef654fd
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 21:56:16 +0530

    Problem Statement Updated
    Second Largest Number

--- C/PA2Q2.C
+++ C/PA2Q2.C
@@ -1,4 +1,14 @@
-/*  Not Using Pointer and Array */
+/*   
+ *	Problem Statement
+ *
+ *	You are given a sequence of integers as input, terminated by a -1. (That is, the input integers may be positive, negative or 0. 
+ *	A -1 in the input signals the end of the input.) -1 is not considered as part of the input.  
+ *	
+ *	Find the second largest number in the input. You may not use arrays. 
+ */
+
+//  Not Using Pointer and Array
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -25,114 +35,3 @@ int main()
     printf("%d\n",secondL);
     return 0;
 }
-/*  First Code Ends Here    */
-
-/*  Pointer */
-#include <stdio.h>
-#include <stdlib.h>
-
-int secondLargest(int *p, int size)
-{
-    int larg=*p;
-    
-    for(int i=1; i<size; i++)
-    {
-        if(*(p+i)!=larg)
-            return *(p+i);
-    }
-    return 0;
-}
-
-void sort(int *p,int size)
-{
-    int temp;
-    
-    for (int i = 0; i < size; ++i) 
-        for (int j = i + 1; j < size; ++j) 
-            if (*(p+i) < *(p+j)) 
-            {
-                temp = *(p+i);
-                *(p+i) = *(p+j);
-                *(p+j) = temp;
-            }
-}
-
-int main()
-{
- 
-    int *arr=NULL;
-    int num=0, count=0, result;
-    
-    scanf("%d",&num);
-    
-    while(num!=-1.0)
-    {
-        arr=(int * )realloc(arr, (count+1)*sizeof(num));
-        *(arr + count)=num;
-        count++;
-        scanf("%d",&num);
-    }   
-    
-    sort(arr,count);
-    
-   	result=secondLargest(arr, count);
-
-    printf("%d\n",result);
-
-    return 0;
-}
-/*  Code Ends Here for Pinter   */
-
-/*  Array */
-#include <stdio.h>
-#include <stdlib.h>
-
-int secondLargest(int p[], int size)
-{
-    int larg=p[0];
-    for(int i=1; i<size; i++)
-    {
-        if(p[i]!=larg)
-            return p[i];
-    }
-    return 0;
-}
-
-void sort(int p[],int size)
-{
-    int temp;
-    
-    for (int i = 0; i < size; ++i) 
-        for (int j = i + 1; j < size; ++j) 
-            if (p[i] < p[j]) 
-            {
-                temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
-            }
-}
-
-int main()
-{
- 
-    int arr[100];
-    int num=0, count=0, result;
-    
-    scanf("%d",&num);
-    
-    while(num!=-1)
-    {
-        arr[count++]=num;
-        scanf("%d",&num);
-    }
-    
-    sort(arr,count);
-
-    result=secondLargest(arr,count);
-
-    printf("%d\n",result);
-
-    return 0;
-}
-/*  Code Ends here for Array    */
-
