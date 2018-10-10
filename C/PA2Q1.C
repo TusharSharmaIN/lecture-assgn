01f7ce681cfb853291e33beb65aead5263d53584
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 21:49:43 +0530

    Problem Statement Updated
    Program to check whether a matrix is triangular or not

--- C/PA2Q1.C
+++ C/PA2Q1.C
@@ -2,15 +2,15 @@
  *	Problem Statement
  *
  *	In this assignment, you will be given an NxN matrix. You have to determine whether the matrix is a triangular matrix. 
-
+ *
  *	The diagonal of the matrix M of size NxN is the set of entries M(0,0), M(1,1), M(2,2), ..., M(N,N). 
-
+ *
  *	A matrix is upper triangular if every entry below the diagonal is 0. For example,  
  *	1 1 1
  *	0 0 1
  *	0 0 2
  *	is an upper triangular matrix. (The diagonal itself, and the entries above and below the diagonals can be zeroes or non-zero integers.) 
-
+ *
  *	A matrix is lower triangular if every entry above the diagonal is
  *	0. For example, 
  *	2 0 0
@@ -31,7 +31,6 @@
  *	------
  *	If the input matrix is triangular, then print yes. Otherwise, print no. 
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
