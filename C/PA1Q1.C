37ca01c710b5cc2e1f8b0c5f6dc2b4e1b9e162aa
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 21:29:28 +0530

    Problem Statement Updated
    Check difference from three given inputs

--- C/PA1Q1.C
+++ C/PA1Q1.C
@@ -1,3 +1,21 @@
+/*   Problem Statement
+ *
+ *   You will be given 3 integers as input. The inputs may or may not be different from each other. 
+ *
+ *   You have to output 1 if all three inputs are different from each other, and 0 if any input is repeated more than once.
+ *
+ *   Input
+ *   -----
+ *   Three integers on three lines.
+ *
+ *   Output
+ *   ------
+ *   1 if the three inputs are different from each other
+ *   , 0 if some input is repeated more than once
+ *    
+ */
+
+
 #include <stdio.h>
 
 int main()
