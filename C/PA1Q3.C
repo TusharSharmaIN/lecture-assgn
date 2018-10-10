2f0704f054b11d8355c7d88523f4ad7c6650509e
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 21:41:38 +0530

    Problem Statement Updated
    Check whether the triangle is possible by given sides as input.

--- C/PA1Q3.C
+++ C/PA1Q3.C
@@ -1,3 +1,20 @@
+/*	
+ *	Problem Statement
+ *	
+ *	A triple of numbers (a,b,c) is called a triangle triple if we can form a triangle of lengths a, b and c.
+ *
+ *	In this question, you will be given three numbers. You have to output 1 if the three numbers can form a triangle. Otherwise, you have to output 0.
+ *
+ *	Input
+ *	-----
+ *	Three real numbers.
+ *	
+ *	Output
+ *	------
+ *	1 if the three numbers can form a triangle 0 otherwise
+ *
+ */
+
 #include <stdio.h>
 
 int main()
