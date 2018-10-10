c3e654d554c82d006ba4fcbe7f332aecaad60d03
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 21:33:25 +0530

    Problem Statement Updated
    Check input number is a multiple of given number

--- C/PA1Q2.C
+++ C/PA1Q2.C
@@ -1,9 +1,27 @@
+/*	
+ *  Problem Satement
+ *  
+ *  You are given two integers, say M and N.
+ *
+ *	You must check whether M is an exact multiple of N, without using
+ *	loops.
+ *	You have to output 0 if M is not a multiple of N.
+ *	You have to output M/N if M is a multiple of N.
+ *
+ *	Input-----
+ *	Two integers, say M and N.
+ *
+ *	Output------
+ *	You have to output 0 if M is not a multiple of N.
+ *	You have to output M/N if M is a multiple of N.
+ */
+
 #include <stdio.h>
 
 int main()
 {
     int m,n;
-    scanf("%d%d%d",&m,&n);
+    scanf("%d %d %d",&m,&n);
     
     if(m%n==0)
         printf("%d",(m/n));
