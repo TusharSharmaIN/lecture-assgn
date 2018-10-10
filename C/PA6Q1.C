26f634df05ac68fe6bfb7e184494d1ad2847b5fe
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 22:45:56 +0530

    Problem Statement Updated
    Count corresponding characters from two strings if they are anagram.

--- C/PA6Q1.C
+++ C/PA6Q1.C
@@ -1,3 +1,23 @@
+/*   
+ *	Problem Statement
+ *
+ *	We say that a string 's' is an anagram of another string 't' if the letters in 's' can be rearranged to form 't'.
+ *	
+ *	For example, "butterfly" is an anagram of "flutterby", since a rearrangement of the first word results in the second.
+ *	
+ *	We say that a position 'i' in 's' and 't' match, if 's' is an anagram of 't', and s[i]==t[i].
+ *	
+ *	In this question, you will be given two words, 's' and 't'. You have to output the number of matching positions if s is an anagram of t, and -1 if s is not an anagram of t.
+ *
+ *	Input
+ *	-----
+ *	The input consists of two lines. The first line contains the first string, with length <= 100 characters. The second line contains the second string, with length <= 100 characters.
+ *	
+ *	Output
+ *	------
+ *	If the first string is an anagram of the second string, then output the number of matching positions. Otherwise, print -1.
+ */
+
 #include <stdio.h>
 #include <ctype.h>
 
