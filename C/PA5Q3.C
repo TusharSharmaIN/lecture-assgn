2dccebbbb8c7b979240a2cc8cb3335f0d511adfa
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 22:40:33 +0530

    Problem Statement Updated
    Reverse of a string using recursion

--- C/PA5Q3.C
+++ C/PA5Q3.C
@@ -1,3 +1,17 @@
+/*   
+ *	Problem Statement
+ *
+ *	Write a recursive program that inputs a line of characters from the user. The line may contain blanks. It outputs the line with the characters reversed. The input ends with EOF (end of file). 
+ *	NOTE: You have to use recursion to solve this, and are NOT allowed to use array to store the input!! 
+ *	
+ *	Example: 
+ *	INPUT 
+ *	This is easy 
+ *	
+ *	OUTPUT 
+ *	ysae si sihT
+ */
+
 #include <stdio.h>
 
 void getString()
