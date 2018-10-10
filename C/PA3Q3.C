f3272c3d48d4b93f9df8146c5f9c745823869388
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 22:13:12 +0530

    Problem Statement Updated
    Count Words separated by EOF characters.

--- C/PA3Q3.C
+++ C/PA3Q3.C
@@ -1,11 +1,45 @@
+/*   
+ *	Problem Statement
+ *
+ *	A line of English text will be given, where words are separated by one of the following symbols:
+ *	
+ *	' ', '\t', '.', ',' and ';'
+ *	
+ *	Each word may be separated from the next and the previous by one or more of the following symbols. You have to count the number of words in the sentence.
+ *
+ *	Note that to read the input, you have to read until the EOF symbol is
+ *	read, as in the following example.
+ *	
+ *	int main()
+ *	{
+ *		int c;
+ *	
+ *		c = getchar();
+ *		while ( c != EOF ) {
+ *			c = getchar();
+ *		}
+ *		return 0;
+ *	}
+ *
+ *	Note: it is possible to solve this question without arrays, so the maximum length of the line is not important.
+ *	
+ *	Input
+	-----
+ *	A line of English text with words separated from one another by one or more occurrences of the symbols
+ *	
+ *	' ', '\t', '.', ',' and ';'
+ *
+ *	Output
+ *	------
+ *	The number of words in the line.
+ */
+
 #include<stdio.h>
 
 int main()
 {
 	char c, d;
-	
 	int count = 0;
-	
 	d = c = getchar();
 	
 	while(c != EOF) 
@@ -16,7 +50,6 @@ int main()
 		if(c != ' ' && c != '\t' && c != '.' && c != ',' && c != ';')
 			if(d == ' ' || d == '\t' || d == '.' || d == ',' || d == ';' || d == EOF)
 				count++;
-		
 		if(d == EOF)
 			break;
 	}
