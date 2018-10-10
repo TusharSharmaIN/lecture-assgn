2577aff8a0a5a113792e05ec4b523b6a195ea50a
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-10 22:04:57 +0530

    Problem Statement Updated
    Find Occurrence of even number

--- C/PA3Q1.C
+++ C/PA3Q1.C
@@ -1,3 +1,29 @@
+/*   
+ *	Problem Statement
+ *
+ *	Write a C function to find the kth occurrence of an even integer in a sequence of non-negative integers, and then call your function from main. 
+ *	
+ *	Your function should be according to the following declaration:
+ *	
+ *	int find_even(int k);
+ *	
+ *	Input
+ *	-----
+ *	You are given the input in two lines:
+ *	
+ *	The first line contains a positive integer k. 
+ *	In the second line, you will be given a sequence of numbers. 
+ *	
+ *	You have to find the kth occurrence of n in the sequence below. 
+ *	
+ *	The second line consists of a sequence of non-negative integers, terminated with a -1.  The -1 is not part of the sequence.
+ *	
+ *	Output
+ *	------
+ *	If there are  k even numbers in the sequence, then output the  kth occurrence of even in the sequence. Otherwise, output a -1.
+ *	
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
