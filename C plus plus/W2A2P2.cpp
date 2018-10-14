82a95584b4dc5394153269a38fad8f1b5fa51576
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-14 17:54:36 +0530

    Problem Statement Updated

--- C plus plus/W2A2P2.cpp
+++ C plus plus/W2A2P2.cpp
@@ -1,3 +1,10 @@
+/*
+ *	Problem Statement
+ *	
+ *	Complete the Macro definition to get the desired output according to the test cases.
+ *
+ */
+
 #include <iostream>
 using namespace std;
 #define SQUARE(x) (x)*(x)-(x-1) // Complete the Macro definition
